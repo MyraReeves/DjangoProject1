5044a7c95b66ccd2a9023a1a8b973882fc509693
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-13 21:44:15 -0700

    Adds array of healthy breakfast items

--- src/mainapp/mainapp/views.py
+++ src/mainapp/mainapp/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     user = request.user
+    products1 = ["Coffee", "Tea", "Fruit juices", "Hard-boiled eggs", "Cereal", "Yogurt", "English muffins", "Bagels", "Cream cheese", "Assorted jams/jellies", "Oatmeal", "Apples, Oranges, or Bananas"]
     context = {
         'user': user,
+        'breakfastProducts':  products1,
     }
     return render(request, "home.html", context)
