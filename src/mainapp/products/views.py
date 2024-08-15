26c87476fae2ef2a2711c4e2ae68c2f638364f3e
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-15 01:48:11 -0700

    Creates function to point to menu webpage

--- src/mainapp/products/views.py
+++ src/mainapp/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
 
 # Create your views here.
+def admin_console(request):
+    menu_data = Product.objects.all()
+    return render(request, 'products/menu.html', {'menu': menu_data})
