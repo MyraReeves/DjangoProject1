9d4b7869f336d570194e7d26b4e30e60d1cdce75
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-19 00:07:38 -0700

    Adds path for calling up a menu item's details page

--- src/mainapp/products/urls.py
+++ src/mainapp/products/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path(url name, name of the method being called, name=nickname)
     path('admin_console', views.admin_console, name='admin_console'),
+    path('<int:pk>/details/', views.details, name='details'),
 ]
